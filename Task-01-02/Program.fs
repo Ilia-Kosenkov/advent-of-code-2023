3ec1e891294fceb1b37c17df5ca24a51a146e53d
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-12-02 18:16:36 +0200

    Add support for numbers spelled out as words

--- Task-01-02/Program.fs
+++ Task-01-02/Program.fs
@@ -1,36 +1,34 @@
 ﻿open System
 open System.Text.RegularExpressions
-let digitRegex = Regex("\\d", RegexOptions.Compiled ||| RegexOptions.NonBacktracking)
+let digitRegex = Regex("\\d|one|two|three|four|five|six|seven|eight|nine", RegexOptions.Compiled ||| RegexOptions.NonBacktracking)
 
 
-// Iterate a `string` once, collecting first and last digit characters
-// Returns either `Some(first, last)` or `None` if no digits found
 let extractDigitCharacters line =
     let matches = line |> digitRegex.Matches
-    Some ((matches |> Seq.head).Value, (matches |> Seq.last).Value)
+    match matches.Count with
+    | 0 -> None
+    | n -> Some(matches[0].Value, matches[n-1].Value)
 
 
 let parseInt (matchedString: string) =
     match matchedString with
-        | "1" -> Some 1
-        | "2" -> Some 2
-        | "3" -> Some 3
-        | "4" -> Some 4
-        | "5" -> Some 5
-        | "6" -> Some 6
-        | "7" -> Some 7
-        | "8" -> Some 8
-        | "9" -> Some 9
+        | "1" | "one" -> Some 1
+        | "2" | "two" -> Some 2
+        | "3" | "three" -> Some 3
+        | "4" | "four" -> Some 4
+        | "5" | "five" -> Some 5
+        | "6" | "six" -> Some 6
+        | "7" | "seven" -> Some 7
+        | "8" | "eight" -> Some 8
+        | "9" | "nine" -> Some 9
         | "0" -> Some 0
         | _ -> None
 
-// Parse an optional pair of digit characters into an optional pair of numbers
 let parseDigits pairOpt =
     match (pairOpt |> Option.map (fun (f, l) -> (parseInt f, parseInt l))) with
     | Some(Some first, Some last) -> Some((first, last))
     | _ -> None
 
-// Convert optional pair of ints into an optional int by combining digits
 let constructNumber pairOpt =
     pairOpt |> Option.map (fun (f, l) -> f * 10 + l)
 
@@ -38,7 +36,7 @@ let constructNumber pairOpt =
 Environment
     .GetCommandLineArgs()
     .[1].Split(Environment.NewLine, StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
-|> Seq.map extractDigitCharacters // Extract digit characters
+|> Seq.map extractDigitCharacters // Extract digit substrings
 |> Seq.map parseDigits // Parse
 |> Seq.map constructNumber // Construct check sums
 |> Seq.fold // Compute sum if all check sums are Some, otherwise return None
