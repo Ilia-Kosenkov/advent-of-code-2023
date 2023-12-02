678e440cd448ed10212ea611f326cd27c35eb1ad
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-12-02 17:41:45 +0200

    Comments

--- Task-1/Program.fs
+++ Task-1/Program.fs
@@ -1,5 +1,7 @@
 ﻿open System
 
+// Iterate a `string` once, collecting first and last digit characters
+// Returns either `Some(first, last)` or `None` if no digits found
 let extractDigitCharacters line =
     match
         (line
@@ -17,21 +19,26 @@ let extractDigitCharacters line =
 
 let parseInt (c: char) = c |> string |> int
 
+// Parse an optional pair of digit characters into an optional pair of numbers
 let parseDigits pairOpt =
     pairOpt |> Option.map (fun (f, l) -> (parseInt f, parseInt l))
 
+// Convert optional pair of ints into an optional int by combining digits
 let constructNumber pairOpt =
     pairOpt |> Option.map (fun (f, l) -> f * 10 + l)
 
-Environment.GetCommandLineArgs().[1].Split(Environment.NewLine)
-|> Seq.map extractDigitCharacters
-|> Seq.map parseDigits
-|> Seq.map constructNumber
-|> Seq.fold
-    (fun sum number ->
-        match (sum, number) with
+// Read input from stdin (new-line-separated string passed as first arg)
+Environment
+    .GetCommandLineArgs()
+    .[1].Split(Environment.NewLine, StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+|> Seq.map extractDigitCharacters // Extract digit characters
+|> Seq.map parseDigits // Parse
+|> Seq.map constructNumber // Construct check sums
+|> Seq.fold // Compute sum if all check sums are Some, otherwise return None
+    (fun sum lineCheckSum ->
+        match (sum, lineCheckSum) with
         | (_, None) -> None
         | (None, _) -> None
-        | (Some s, Some n) -> Some(s + n))
-    (Some 0)
+        | (Some s, Some lcs) -> Some(s + lcs))
+    (Some 0) // Start with 0. Is any of the check sums are None, accumulator is replaced with None
 |> printfn "%A"
