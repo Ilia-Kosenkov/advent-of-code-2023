38cd8eb8cebafb472dfe8aadb116fd026af03a93
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-12-02 17:49:57 +0200

    Update runner

--- run.ps1
+++ run.ps1
@@ -1,4 +1,7 @@
-param([int]$TaskId, [string]$InputFilePath)
+param([int]$Task, [int]$Part,  [string]$InputFilePath)
 
 $content = Get-Content -Path $InputFilePath -Raw
-dotnet run --project "Task-$TaskId/Task-$TaskId.fsproj" -- $content
+$idStr = $Task.ToString("00")
+$partId = $Part.ToString("00")
+
+dotnet run --project "Task-$idStr-$partId/Task-$idStr-$partId.fsproj" -- $content
