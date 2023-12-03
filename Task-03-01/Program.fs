a14f333e07628ed18001c6fcf18e4c4cbc5546b2
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-12-03 15:18:15 +0200

    Setup task 03

---
+++ Task-03-01/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+
+let splitOptions =
+    StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries
+
+Environment.GetCommandLineArgs().[1].Split(Environment.NewLine, splitOptions)
+|> Array.iter (printfn "%A")
